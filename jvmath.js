7cc39ae2ee0e338477cd11cd791251614b72243d
yvonne mare <yvonne@mare.co.zw>
2019-02-28 15:45:35 +0200

    javascript
    applying variables in javascript to design a website.

---
+++ jvmath.js
@@ -0,0 +1,7 @@
+var x,y;
+x=5;
+y=6;
+  document.getElementById("demo").innerHTML=x+y;
+  document.getElementById("mathone").innerHTML=x-y;
+  document.getElementById("mathtwo").innerHTML=x*y;
+  document.getElementById("maththree").innerHTML=x/y;
