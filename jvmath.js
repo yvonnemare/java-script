677d57bbd0a52c4c1dc181cb3156df0d5f43cbf1
yvonne mare <yvonne@mare.co.zw>
2019-03-04 12:50:27 +0200

    arrays revision

--- jvmath.js
+++ jvmath.js
@@ -5,3 +5,6 @@ y=6;
   document.getElementById("mathone").innerHTML=x-y;
   document.getElementById("mathtwo").innerHTML=x*y;
   document.getElementById("maththree").innerHTML=x/y;
+
+
+document.write("<ul>"+"<li>"+"Harvey"+</li>+<li>+"has"</li>+<li>12</li>+<li>"cars"</li>);
