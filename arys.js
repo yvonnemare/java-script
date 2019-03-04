253f81451e70e5768cd3403956de7e10a3ffca95
yvonne mare <yvonne@mare.co.zw>
2019-03-04 13:12:35 +0200

    using push aray
    using push in javascript!

--- arys.js
+++ arys.js
@@ -1,3 +1,3 @@
-var things=[ 23, " cars", " harvey", 11];
-
-document.getElementByID("demo").innerHTML="<ul>" + "<li>" + things[2] + "</li>" + "<li>" + "has" + "</li>" + "<li>" + (things[0]-things[3]) + "</li>"+ "<li>" + things[1] + "</li>" + "</ul>";
+var things=[23, " cars", " harvey", 11];
+things.push("twelve");
+document.getElementById("demo").innerHTML = "<ul>" + "<li>" + things[2] + "</li>" + "<li>" + "has" + "</li>" + "<li>" + things[4] + "("+ (things[0] - things[3])+")" + "</li>"+ "<li>" + things[1] + "</li>" + "</ul>";
