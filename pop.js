9357e2feaf3ad7d1f347fa6ebf04386cdf236438
yvonne mare <yvonne@mare.co.zw>
2019-03-04 14:07:54 +0200

    unshift and shift array

--- pop.js
+++ pop.js
@@ -2,10 +2,10 @@ var cars=[" BMW", " benz", " toyota"];
 
 document.getElementById("demo").innerHTML="There are" + cars.length+ " cars..." + cars.toString();
 
-cars.push("Land Rover");
+cars.unshift("Lexus");
 
 document.getElementById("rov").innerHTML="There are now" + cars.length+ " cars..."+cars.toString();
 
-cars.pop();
-cars.pop();
+cars.shift();
+cars.shift();
 document.getElementById("rm").innerHTML="There are now" +cars.length+ "cars..."+cars.toString();
