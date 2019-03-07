6b25c9f2142f89d81eb6a309dbada29f37302657
yvonne mare <yvonne@mare.co.zw>
2019-03-07 11:23:38 +0200

    if condition
    using if condition

--- if.js
+++ if.js
@@ -1,7 +1,17 @@
-var days = ["Monday", "Tue", "Wed",];
+var days = ["Monday", " Tue", " Wed",];
 if(days[0] == "Monday"){
 
-  document.write("Hey l found Monday");
+  document.write("Hey l found Monday <br> ");
 }else{
-  document.write("Bummer man, no Monday here");
+  document.write("Bummer man, no Monday here <br> ");
 }
+if (days[1] == " Tuesday"){
+  document.write("Hey l found Tuesday <br> ");
+}else {
+  document.write("Bummer man, no Tuesday here <br> ");
+}
+if(days[2] == "Wednesday"){
+  document.write("Hey i found Wednesday <br> ");
+}else{
+    document.write("Bummer man, no Wednesday here");
+  }
---
+++ switch.js
@@ -0,0 +1,13 @@
+var cars = ["BMW", "Land Rover", "Skoroskoro", "Lexus"];
+
+switch(cars[2]){
+case "Land Rover"
+document.write("Land Rovers are here");
+break;
+case "Lexus"
+document.write("Lexus are here");
+break;
+default :
+document.write("No Lexus or Land Rover ")
+
+}
