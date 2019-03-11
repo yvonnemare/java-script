6e309622433bc0906a5f66d31eb440f37d93c6d2
yvonne mare <yvonne@mare.co.zw>
2019-03-11 12:27:51 +0200

    objects
    l used objects in javascript!

---
+++ objct.js
@@ -0,0 +1,10 @@
+var car = {
+
+  brand: " Toyota",
+  make: " Corolla",
+  Type:" Sedan",
+  Color:" blue",
+  yearOfRelease:"1997",
+};
+document.getElementById("demo").innerHTML="This is a " +car.brand +"car called a" +car.make;
+document.getElementById("dem").innerHTML="It is a "+ car.Type + "which is" + car.Color + " and was released in" + car.yearOfRelease;
