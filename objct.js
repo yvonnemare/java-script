a0466de133ef1cb15b005a392cc4584f4ac8a5d1
yvonne mare <yvonne@mare.co.zw>
2019-03-11 15:52:22 +0200

    revision exercise
    hey that was a revision!

--- objct.js
+++ objct.js
@@ -13,9 +13,22 @@ var corolla ={
     yearOfRelease: 2010
 };
 
-var cars =[ corolla,  rangeRover];
 
-for(i=0; i<cars.length; i++){
-document.write("This is a " + cars[i].brand + " car called a " + cars[i].make + ". It is a " + cars[i].type + " which is " + cars[i].color + " and was released in " +
-cars[i].yearOfRelease   + "<br/><br/>");
+
+var pickUp = {
+  brand: " Nissan",
+  make: " Hard body",
+  type: " Pick Up Truck",
+  color: " Grey",
+  yearOfRelease: 2003
+};
+var cars =[ corolla,  rangeRover];
+cars.unshift(pickUp);
+var i=0;
+
+do{
+  document.write("This is a " + cars[i].brand + " car called a " + cars[i].make +  ". <br> It is a " + cars[i].type + " which is " + cars[i].color +
+" and was released in " + cars[i].yearOfRelease  + "<br/><br/>");
+i++
 }
+while(i<cars.length);
